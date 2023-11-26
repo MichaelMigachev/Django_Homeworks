001d0b00f96ca0741b37b2da727954cbdf9a8b34
MichaelMigachev <migachev21@gmail.com>
2023-11-26 11:15:16 +0300

    выполнил Д/З 19.2

---
+++ catalog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        data = {'name': name, 'phone': phone, 'message': message}
+        with open('data.json', 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False)
+
+
+    return render(request, 'catalog/contacts.html')
